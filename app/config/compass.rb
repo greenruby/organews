21b8521a24ef0cce5fff4270a90530213d7fd41e
mose <mose@mose.com>
2013-07-01 02:27:01 +0800

    preparing grn 21, and some cleanup

--- app/config/compass.rb
+++ app/config/compass.rb
@@ -10,6 +10,7 @@ javascripts_dir = "static/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
