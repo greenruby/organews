6f4284733e957639813057d2884ce9960f0b1bd3
eguitarz <dalema22@gmail.com>
2013-08-04 21:55:38 +0800

    refresh feeds when user querying

--- app.rb
+++ app.rb
@@ -39,12 +39,34 @@ class App < Sinatra::Base
     erb :index
   end
 
-  get '/v1/:thing' do
-    DB.collection(params[:thing]).find.to_a.map{ |t| frombsonid(t) }.to_json
-  end
-
-  get '/v1/:thing/:id' do
-    frombsonid(DB.collection(params[:thing]).find_one(tobsonid(params[:id]))).to_json
+  # will triggger scrapers if need
+  get '/v1/feeds' do
+    feeds = DB.collection('feeds').find.to_a.map{ |t| frombsonid(t) }
+    updated = false
+    feeds.each do |f| 
+      if Time.now - f['updated_at'] > 0
+        begin
+          rss = JSON.parse RSS.new(f['url']).to_json
+        rescue
+          break
+        end
+        new_items = rss['items'].select do |i|
+          f['items'].select{ |old_i| old_i['digest'] == i['digest'] }.size == 0
+        end
+        unless new_items.empty?
+          updated = true
+          DB.collection('feeds').update(
+            { '_id' => tobsonid(f['id']) }, 
+            { 
+              '$push' => { 'items' => { '$each' => new_items } ,
+              '$set' => { 'updated_at' => Time.now }
+            }
+          )
+        end
+      end
+    end
+    feeds = DB.collection('feeds').find.to_a.map{ |t| frombsonid(t) } if updated
+    feeds.to_json
   end
 
   post '/v1/feeds' do
@@ -54,13 +76,23 @@ class App < Sinatra::Base
     rss = JSON.parse(RSS.new(params[:url]).to_json)
     hash = {}
     hash['title'] = rss['channel']
+    hash['url'] = rss['url']
     hash['items'] = rss['items']
-    hash['items'].map {|i|
-      i['published_at'] = Time.now if i['published_at'].nil? || i['published_at'].empty?
-    }
+    # hash['items'].map {|i|
+    #   i['published_at'] = Time.now if i['published_at'].nil? || i['published_at'].empty?
+    # }
     hash['created_at'] = Time.now
+    hash['updated_at'] = Time.now
     oid = DB.collection("feeds").insert(hash)
     "{\"id\": \"#{oid.to_s}\"}"
+  end
+
+  get '/v1/:thing' do
+    DB.collection(params[:thing]).find.to_a.map{ |t| frombsonid(t) }.to_json
+  end
+
+  get '/v1/:thing/:id' do
+    frombsonid(DB.collection(params[:thing]).find_one(tobsonid(params[:id]))).to_json
   end
 
   post '/v1/:thing' do
