9b3c783b995c092929f10eff660be81038eae4b7
mose <mose@mose.com>
2014-02-16 14:13:12 +0800

    more setup cleaning

--- lib/organews/cli.rb
+++ lib/organews/cli.rb
@@ -3,7 +3,7 @@ module Organews
     include Thor::Actions
 
     def self.source_root
-      File.expand_path("../../../templates", __FILE__)
+      File.expand_path("../../..", __FILE__)
     end
 
     default_task :help
@@ -12,6 +12,11 @@ module Organews
       banner: "PATH",
       default: File.expand_path("config.yml", Dir.pwd),
       desc: "Path to the configuration file to use"
+
+    desc "init", "creates base tree for organews."
+    def init
+      directory "templates", "./"
+    end
 
   end
 end
