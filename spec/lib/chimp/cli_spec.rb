f716ebb650a9cb6d9bdb3b7f0d2eb57f19315e78
mose <mose@mose.com>
2014-02-25 23:50:35 +0800

    prepare tests for the chimp api client

---
+++ spec/lib/chimp/cli_spec.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+require 'spec_helper'
+require "organews/chimp/cli"
+
+describe Organews::Chimp::Cli do
+
+end
---
+++ spec/lib/chimp/client_spec.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+require 'spec_helper'
+require "organews/chimp/client"
+
+describe Organews::Chimp::Client do
+
+end
